1c6b587c4c03b14569592fe042bec096803bde1e
victormora1 <victorjmora1@gmail.com>
2023-03-26 22:20:43 -0400

    redid the variable thatis going to be used

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const rowTimeBlockPast = document.getElementById()
+// name the var you are going to use
+const rowTimeBlockPast = document.getElementById();
+const rowTimeBlockPresent = documentgetElementById();
+const rowTimeBlockFuture = documentgetElementById();
+const description = documentgetElementById();
+
+if (hour < currentHour) {
+    timeBlock.addClass("past");
+} else if(hour === currentHour) {
+    timeBlock.addClass("present");
+} else {
+    timeBlock.addClass("future")
+}
